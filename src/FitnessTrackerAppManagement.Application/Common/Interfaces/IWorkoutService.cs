aab42a08698f611c33e2d606de4ccfd87f2c29bb
Danielle <daniell07720@gmail.com>
2025-07-11 03:42:55 +0800

    feat: added IWorkoutService

---
+++ src/FitnessTrackerAppManagement.Application/Common/Interfaces/IWorkoutService.cs
@@ -0,0 +1,19 @@
+﻿using FitnessTrackerAppManagement.Application.Common.DTOs.Requests;
+using FitnessTrackerAppManagement.Application.Common.DTOs.Responses;
+
+namespace FitnessTrackerAppManagement.Application.Common.Interfaces
+{
+    public interface IWorkoutService
+    {
+        #region Private Methods
+
+        Task<WorkoutDto> CreateWorkoutAsync(WorkoutCreateRequest request);
+        Task<List<WorkoutDto>> GetAllWorkoutsAsync();
+        Task<WorkoutDto?> GetWorkoutByIdAsync(int id);
+        Task<bool> UpdateWorkoutAsync(int id, WorkoutUpdateRequest request);
+        Task<bool> DeleteWorkoutAsync(int id);
+
+
+        #endregion Private Methods
+    }
+}
