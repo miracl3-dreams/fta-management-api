6dba1dc7fc3d5e293fe304636e7c0ad86ae521bd
Danielle <daniell07720@gmail.com>
2025-07-11 03:43:12 +0800

    feat: added WorkoutResult

--- src/FitnessTrackerAppManagement.Application/Common/Results/WorkoutResult.cs
+++ src/FitnessTrackerAppManagement.Application/Common/Results/WorkoutResult.cs
@@ -1,6 +1,18 @@
 ﻿namespace FitnessTrackerAppManagement.Application.Common.Results
 {
-    internal class WorkoutResult
+    public class WorkoutResult
     {
+        #region Properties
+
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int DurationInMinutes { get; set; }
+        public int CaloriesBurned { get; set; }
+        public string IntensityLevel { get; set; } = string.Empty;
+        public string Status { get; set; } = "Planned";
+
+        #endregion Properties
     }
 }
